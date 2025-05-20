e21d52e7a547c9272e5100ed86aa6c507c21b9d4
Felipe OMC <83432178+Felipeomc@users.noreply.github.com>
2025-05-20 10:37:08 -0300

    Update brute_force.py

--- ga_bn_calibration/brute_force.py
+++ ga_bn_calibration/brute_force.py
@@ -1,15 +1,14 @@
+
 import time
 import numpy as np
 from sklearn.metrics import mean_squared_error
-from bn_as_fitness_v2 import mix_and_transform_with_tnormal,  functions
+from bn_as_fitness_v2_translated import mix_and_transform_with_tnormal,  functions
 import itertools
 
-
 # ============================
-# Tabela com os dados do especialista
+# expert data
 # ============================
 expert_data = [
-                               
     {"AT": "VL", "AC": "VH", "AE_expert": [0.274, 0.323, 0.274, 0.081, 0.048]},
     {"AT": "VH", "AC": "VL", "AE_expert": [0.172, 0.259, 0.345, 0.172, 0.052]},
     {"AT": "VL",  "AC": "VL",  "AE_expert": [0.333, 0.333, 0.283, 0.050, 0.0]},
@@ -19,66 +18,53 @@ expert_data = [
 ]
 
 
-# Geração do repositório de amostras
-#repositorio = gerar_amostras_base_por_estado()
+
 import pickle
-with open('repositorio.pkl', 'rb') as f:
-    repositorio = pickle.load(f)
+with open('repository.pkl', 'rb') as f:
+    repository = pickle.load(f)
 
-pesos_possiveis = list(itertools.product(range(1, 6), repeat=2))  # gerar pesos possiveis para 2 pais
-#pesos_possiveis = list(itertools.product(range(1, 6), repeat=3)) #pra 3 pais
+possible_weights = list(itertools.product(range(1, 6), repeat=2))  
 
-variancias = [0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5]
+variances = [0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5]
 
+start = time.time()
 
-
-inicio = time.time()
-
-melhor_erro = float("inf")
-melhor_config = {}
-print("Iniciando calibração por força bruta...\n")
+best_error = float("inf")
+best_config = {}
+print("begin brute force ...\n")
 
 for nome_func, func in functions.items():
-  
-    for pesos in pesos_possiveis:
-        variancias = [0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5]
-        for variance in variancias:
-            erros = []
+    for weights in possible_weights:
+        variances = [0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5]
+        for variance in variances:
+            errors = []
             for c in expert_data:
-                estados_pais = [c["AT"], c["AC"]]  #  testando com 2 pais
-                    
+                parent_states = [c["AT"], c["AC"]]
                 probs_model = mix_and_transform_with_tnormal(
-                    estados_pais, 
-                    pesos, 
-                    repositorio,
+                    parent_states,
+                    weights,
+                    repository,
                     variance=round(variance, 2),
                     func_comb=func
                 )
-                erro = mean_squared_error(c["AE_expert"], probs_model)
-                erros.append(erro)
-            media_brier = np.mean(erros)
-            # LOG de cada combinação testada
-            #print(f"Função: {nome_func:<10} | Estados_pais: {estados_pais} | Pesos: {pesos} | Variância: {variance:.2f} | Brier Médio: {media_brier:.5f}")
+                error = mean_squared_error(c["AE_expert"], probs_model)
+                errors.append(error)
+            mean_brier = np.mean(errors)
 
-
-            if media_brier < melhor_erro:
-                melhor_erro = media_brier
-                melhor_config = {
-                    "funcao": nome_func,
-                    "pesos": pesos,
+            if mean_brier < best_error:
+                best_error = mean_brier
+                best_config = {
+                    "function": nome_func,
+                    "weights": weights,
                     "variance": round(variance, 2),
-                    "brier_medio": round(media_brier, 8)
+                    "mean_brier": round(mean_brier, 8)
                 }
 
-print("Melhor configuração encontrada:")
-print(melhor_config)
-# Fim da medição
-fim = time.time()
+print("Best config found:")
+print(best_config)
 
-# ⏱ Resultado
+end = time.time()
 
-print(f"Tempo de execução força bruta: {fim - inicio:.4f} segundos")
+print(f"execution time: {end - start:.4f} sec")
 
-minutos = int((fim - inicio) // 60)
-segundos = (fim - inicio) % 60
-print(f"Tempo de execução: {minutos} minutos e {segundos:.2f} segundos")
+
